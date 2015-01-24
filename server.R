fa829cf43a441f378a5b354268f480356982ae6a
jphaneuf <phaneuf.joe@gmail.com>
2015-01-24 09:42:18 -0800

    deployed to shinyapps.io

--- server.R
+++ server.R
@@ -11,12 +11,12 @@ shinyServer(
       y<-sin(2*pi*time*sf)+rnorm(n,mean=0,sd=wn)
       #@1000us = 1ms, f=1k, >2pi  1000/1000
       par(mfrow=c(2,1))
-      plot(time,y,type="l",col="red",ylim=c(-2,2))
-      titleStr <- paste0("sine wave, amplitude of 1 at ",as.character(sf),"Hz")
+      plot(time,y,type="l",col="red",ylim=c(-2,2),xlab="Time (Seconds)")
+      titleStr <- paste0("sine wave, amplitude of 1 at ",as.character(sf),"Hz, plus noise")
       title(titleStr)
       fftDf <- data.frame(frequency=(1:(n/2))*1e6/4096,signalFFT=abs(fft(y))[1:(n/2)]/(n/2))
-      with(fftDf[1:500,],plot(frequency,signalFFT,xlab="Frequency(Hz)",type="l"))
-      #plot((1:(n/2))*1e6/4096,abs(fft(y))[1:(n/2)]/(n/2),type="l",lwd=3)
+      with(fftDf[1:500,],plot(frequency,signalFFT,xlab="Frequency(Hz)",ylab="FFT(y)",type="l"))
+      title("FFT")
     })
   }
 )
--- ui.R
+++ ui.R
@@ -3,12 +3,16 @@
 
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("Example plot"),
+  headerPanel("FFT Sliderzzz"),
   sidebarPanel(
     sliderInput('fr','choose frequency',value=244,min=200,max=1e5,step=1),
     sliderInput('wn','Add noise to signal',value=0,min=0,max=3,step=.01)
   ),
   mainPanel(
+    p("Adjust the frequency slider to change the frequency of your signal"),
+    p("Adjust the noise slider to punch your signal in the face"),
+    p("Observe your signal magically emerging from  the noise in the frequency domain"),
+    p("Is it magic?  No.  It's a Fourier Transform."),
     plotOutput('newHist')
   )
 ))
